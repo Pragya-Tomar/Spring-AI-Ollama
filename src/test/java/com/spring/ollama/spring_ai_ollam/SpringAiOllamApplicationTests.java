7a9866de2ec6e119e82f449c5a4389e1c768cdf3
Pragya Tomar <56336816+Pragya-Tomar@users.noreply.github.com>
2026-07-05 18:00:58 +0000

    EoD Progress

---
+++ src/test/java/com/spring/ollama/spring_ai_ollam/SpringAiOllamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ollama.spring_ai_ollam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOllamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
